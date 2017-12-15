7b977d6c065e3d9223768b831ace0475a93ffccb
aciremaaleman <aciremaaleman@outlook.es>
2017-12-15 15:23:22 -0600

    Subiendo Archivos de DashBoard

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/*
+ * Funcionalidad de tu producto
+ */
+
+// Puedes hacer uso de la base de datos a través de la variable `data`
+
+
+
+//
+
+//console.log(data['CDMX']['2017-1']['students']);
+//console.log(data['CDMX']['2017-2']['students']);
+
+//console.log(data['AQP']['2016-2']['students']);
+//console.log(data['AQP']['2017-1']['students']);
+
+//console.log(data['LIM']['2016-2']['students']);
+//console.log(data['LIM']['2017-1']['students']);
+//console.log(data['LIM']['2017-2']['students']);
+
+//console.log(data['SCL']['2016-2']['students']);
+//console.log(data['SCL']['2017-1']['students']);
+//console.log(data['SCL']['2017-2']['students']);

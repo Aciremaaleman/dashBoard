40d90b7680d59ab2b3083673e81daed650e13ab8
aciremaaleman <aciremaaleman@outlook.es>
2017-12-20 00:16:07 -0600

    Actualizando overview html

--- js/appOver.js
+++ js/appOver.js
@@ -1,18 +1,35 @@
-document.getElementById('npsStudent');
-
-function nps() {
-  var npsS = data['AQP']['2016-2']['ratings'];
-  for (var k = 0; k < nps.length; k++){
-    var npsprom = npsS[k]['promoters'];
-    var npspassive = npsS[k]['passive'];
-    var npsdetrac = npsS[k]['detractors'];
-
-    var boxG = document.getElementById('general');
-    var 
-
+var pagina = function() {
+  var tabElementos = document.getElementsByClassName('tabs');
+  for(var i = 0; i < tabElementos.length; i++){
+    tabElementos[i].addEventListener('click',mostrar);
   }
-   console.log(npsS);
-   console.log(npsprom);
-   console.log(npspassive);
-   console.log(npsdetrac);
 }
+var mostrar = function(event){
+  var tabSeleccionado = event.target.dataset.tabSeleccionado;
+  var tabuno = document.getElementById('infouno');
+  var tabdos = document.getElementById('infodos');
+  var tabtres = document.getElementById('infotres');
+  var tabcuatro = document.getElementById('infocuatro');
+  if(tabSeleccionado === 'tabuno'){
+    tabdos.style.display = 'none';
+    tabtres.style.display = 'none';
+    tabuno.style.display = 'block';
+    tabcuatro.style.display = 'none';
+  } else if (tabSeleccionado === 'tabdos'){
+    tabtres.style.display = 'none';
+    tabuno.style.display = 'none';
+    tabdos.style.display = 'block';
+    tabcuatro.style.display = 'none';
+  } else if (tabSeleccionado === 'tabtres'){
+    tabuno.style.display = 'none';
+    tabdos.style.display = 'none';
+    tabtres.style.display = 'block';
+    tabcuatro.style.display = 'none';
+  } else if (tabSeleccionado === 'tabcuatro') {
+    tabuno.style.display = 'none';
+    tabdos.style.display = 'none';
+    tabtres.style.display = 'none';
+    tabcuatro.style.display = 'block';
+  }
+}
+pagina();
